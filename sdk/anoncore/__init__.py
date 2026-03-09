94d492a39690f1664171e2e599a06a705466bac2
Md Oshama Bin Nur <69375082+mdobns@users.noreply.github.com>
2026-03-09 14:11:37 +0600

    Initial SDK structure

--- sdk/anoncore/__init__.py
+++ sdk/anoncore/__init__.py
@@ -1,3 +1,38 @@
-# AnonCore SDK
+"""
+AnonCore Python SDK
+A production-ready SDK for the AnonCore anonymous posting platform.
+Implements the Ghost Protocol with Double-Token System.
+"""
 
-This module contains the main exports for the AnonCore SDK.
+from .client import AnonCore
+from .models import (
+    SessionState,
+    Post,
+    Comment,
+    Event,
+    User,
+    Notification,
+)
+from .exceptions import (
+    AnonCoreError,
+    AuthenticationError,
+    ConnectionError,
+    ModerationError,
+    SessionError,
+)
+
+__version__ = "0.1.0"
+__all__ = [
+    "AnonCore",
+    "SessionState",
+    "Post",
+    "Comment",
+    "Event",
+    "User",
+    "Notification",
+    "AnonCoreError",
+    "AuthenticationError",
+    "ConnectionError",
+    "ModerationError",
+    "SessionError",
+]
